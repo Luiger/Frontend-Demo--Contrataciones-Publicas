d7c7155478140cbc2b556f558216aabae6242ded
Luiger <luiger852@gmail.com>
2026-02-04 14:25:35 -0400

    eliminando services por el momento

--- src/services/authService.ts
+++
@@ -1,9 +0,0 @@
-import apiClient from "./apiClient";
-import { LoginCredentials, LoginResponse } from "@/types/auth";
-
-export const authService = {
-  login: async (credentials: LoginCredentials): Promise<LoginResponse> => {
-    const response = await apiClient.post<LoginResponse>("/login", credentials);
-    return response.data;
-  },
-};
